c821671c094d3fe51ff520de2d0b49d7242017ef
Carlo Schmitt <carloschmitt@Carlos-MBP.wg.local>
2022-01-09 14:38:09 +0100

    spielerei mit testdaten

---
+++ ExampleData/convert.py
@@ -0,0 +1,227 @@
+import csv
+import os
+import math
+
+from numpy import double
+
+SAMPLES_PER_SECOND = 125
+WINDOW_SIZE_IN_S = 3
+QUATERNIONS_PER_SAMPLE = 4
+SAMPLES_PER_WINDOW = SAMPLES_PER_SECOND*WINDOW_SIZE_IN_S
+
+SENSOR_A_NAME = "Hand"
+SENSOR_B_NAME = "Hosentasche"
+
+
+def readFile():
+    directory = 'input/'
+    return_data = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            print("datei " + filename + " gefunden...")
+            file = os.path.join(directory, filename)
+            with open(file, newline='') as input:
+                reader = csv.reader(input)
+                data = list(reader)
+                return_data.extend(data[1:])
+    return return_data
+            
+
+#Macht aus eingelesenen Daten Fenster für Ein-Sensor-Training
+def TurnDataIntoWindows(data):
+    windows = []
+    for line in data:
+        
+        name = line[0]
+        pure_data = line[3:]
+        i = 0
+        while i < (len(pure_data) - SAMPLES_PER_WINDOW):
+            new = []
+            new.append(name)
+            new.extend(pure_data[i:i+SAMPLES_PER_WINDOW])
+            i = i+1
+            windows.append(new)
+            #print("Fenstergröße ist: " + str(len(new)))
+
+        print("Aus Bewegung " + name + " " + str(i) + " Fenster erstellt")
+    
+    print("Insgesamt " + str(len(windows)) + " Fenster generiert")
+    return windows
+
+#Holt Daten eines des angegebenen Sensors 
+def getDataForSensor(data, SensorName):
+    returnData = []
+    for line in data:
+        if line[1] == SensorName:
+            returnData.append(line)
+    return returnData
+
+#Schreibt Fenster in eine Datei. 
+def writeWindowsToFile(windows, filename):
+        with open("output/" + filename, "w", newline="") as fileoutput:
+            writer = csv.writer(fileoutput)
+            writer.writerows(windows)
+
+#Prüft ob die Fenster in valider Fensterform sind
+def checkIfValid(windows):
+    returnval = True
+    for window in windows:
+        if len(window) != 601:
+            returnval = False
+    return returnval
+
+#Nimmt zwei Windows auf und gibt kombiniertes Traingsdatenset zurück
+def getDataForSensors(data, sensor1, sensor2):
+    returnData = []
+    for line in data:
+        if line[1] == sensor1:
+            for searchline in data:
+                if line[2] == searchline[2] and searchline[1] == sensor2:
+                    print("bla")
+
+
+def getWindowsFromLine(line):
+    windows = []
+    name = line[0]
+    pure_data = line[3:]
+    i = 0
+    while i < (len(pure_data) - SAMPLES_PER_WINDOW*QUATERNIONS_PER_SAMPLE):
+        new = []
+        new.append(name)
+        new.extend(pure_data[i:i+SAMPLES_PER_WINDOW*QUATERNIONS_PER_SAMPLE])
+        i = i+QUATERNIONS_PER_SAMPLE
+        windows.append(new)
+        #print("Fenstergröße ist: " + str(len(new)))
+    return windows
+
+def combineWindows(a_windows, b_windows):
+    #print("-----> A: " + str(len(a_windows)) + " B: " + str(len(b_windows)))
+    windows = []
+    i = 0
+    for i in range(0, len(a_windows)):
+        #print("->A: " + str(len(a_windows[i])) + " B: " + str(len(b_windows[i])))    
+        kombo_window = []
+        kombo_window.extend(a_windows[i])
+        #print(kombo_window)
+        kombo_window.extend(b_windows[i][1:])
+        #print(len(kombo_window))
+        windows.append(kombo_window)
+        i = i+1
+    return windows
+
+def generateWindowHeader(sensor_count):
+    header = []
+    header.append("MovementName")
+    for i in range(0, sensor_count):
+        for j in range(0, SAMPLES_PER_SECOND*WINDOW_SIZE_IN_S):
+            header.append("S" + str(i) + "_x" + str(j))
+            header.append("S" + str(i) + "_y" + str(j))
+            header.append("S" + str(i) + "_z" + str(j))
+            header.append("S" + str(i) + "_w" + str(j))
+    return header
+
+def differentiateWindows(windows):
+    for window in windows[1:]:
+        for i in range(len(window)-4, 0, -4):
+            if i < 10:
+                print(i)
+            if i == 1:
+                window[1] = 0
+                window[2] = 0
+                window[3] = 0
+                window[4] = 0
+            else:
+                window[i+0] = float(window[i+0]) - float(window[i-4+0])
+                window[i+1] = float(window[i+1]) - float(window[i-4+1])
+                window[i+2] = float(window[i+2]) - float(window[i-4+2])
+                window[i+3] = float(window[i+3]) - float(window[i-4+3])
+    return windows
+
+
+"""
+Data:           MovementName, SensorName, Recording_id, [Sensordaten] 
+Window:         Movementname, 2400x[Sensordaten]
+DualWindow:     Movementname, 2400x[Sensordaten_1], 2400x[Sensordaten_2]
+"""
+
+"""
+Was ist mein Input?
+    Eine Datei pro Bewegung mit Sensordaten von allen Sensoren.
+
+Was soll mein Output sein nach der Vorverarbeitung?
+    3 Ordner mit Trainingsdaten
+        Handgelenk
+            Eine Datei mit Trainingsdaten (Window)
+        Gürtel
+            Eine Datei mit Trainigsdaten (Window)
+        Kombiniert
+            Eine Datei mit Trainingsdaten (DualWindow)
+"""
+
+
+'''
+Pseudo Algorithmus
+
+    Wir wollen drei Sets voll bekommen A, B und Kombo
+    Wir gehen in die Datei Rein
+    In jeder Zeile
+        Namen der Bewegung erfassen
+        Wenn die Daten von Sensor A kommen:
+            Gucken ob Sensor B das gegenstück hat.
+                Windows für Sensor A holen.
+                Windows für Sensor B holen.
+                aus den beiden die Kombo Windows 
+        
+'''
+
+data = readFile()
+
+SensorA_Windows = []
+SensorB_Windows = []
+SensorAB_Windows = []
+
+SensorA_Windows.append(generateWindowHeader(1))
+SensorB_Windows.append(generateWindowHeader(1))
+SensorAB_Windows.append(generateWindowHeader(2))
+
+for line in data:
+    movemnt_name = line[0]
+    sensor_name = line[1]
+    recording_id = line[2]
+    
+    if sensor_name == SENSOR_A_NAME:
+        for searchline in data:
+            if searchline[0] == movemnt_name and searchline[1] == SENSOR_B_NAME and searchline[2] == recording_id:
+                print("--> Datenpaar gefunden! Movement: " + movemnt_name + " rec_id: " + recording_id) 
+                a_windows = getWindowsFromLine(line)
+                print(str(len(a_windows)) + " Windows für Sensor A extrahiert")
+                b_windows = getWindowsFromLine(searchline)
+                print(str(len(b_windows)) + " Windows für Sensor B extrahiert")
+                kombo_windows = combineWindows(a_windows, b_windows)
+                print(str(len(kombo_windows)) +" Windows für Sensoren A,B kombiniert")
+
+                SensorA_Windows.extend(a_windows)
+                SensorB_Windows.extend(b_windows)
+                SensorAB_Windows.extend(kombo_windows)
+                print("Daten angehängt")
+                print("Insgesamt Windows für  A: " + str(len(SensorA_Windows)))
+                print("Insgesamt Windows für  B: " + str(len(SensorB_Windows)))
+                print("Insgesamt Windows für AB: " + str(len(SensorAB_Windows)))
+
+"""
+print("Länge  A: " + str(len(SensorA_Data)))
+print("Länge  B: " + str(len(SensorB_Data)))
+print("Länge  Kombo: " + str(len(Kombo_Data)))
+"""
+SensorA_Windows = differentiateWindows(SensorA_Windows)
+print("Schreibe Sensor A trainingsdaten...")
+writeWindowsToFile(SensorA_Windows, "SensorA.csv")
+#SensorB_Windows = differentiateWindows(SensorB_Windows)
+#print("Schreibe Sensor B trainingsdaten...")
+#writeWindowsToFile(SensorB_Windows, "SensorB.csv")
+#SensorAB_Windows = differentiateWindows(SensorAB_Windows)
+#print("Schreibe Sensor AB trainingsdaten...")
+#writeWindowsToFile(SensorAB_Windows, "SensorsAB.csv")
+
+"""
+"""
---
+++ train.py
@@ -0,0 +1,144 @@
+import os
+import tensorflow as tf
+import cProfile
+import numpy as np
+import pandas as pd
+
+from sklearn.datasets import make_multilabel_classification
+from sklearn.model_selection import train_test_split
+
+from scipy.special import softmax
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+
+df = pd.read_csv("ExampleData/output/SensorA.csv")
+    
+
+'''
+df['Dancen'] = [
+    1 if MovementName == "dancen" else 0 for MovementName in df['MovementName']
+]
+
+df['Dancen'] = [
+    1 if MovementName == "dancen" else 0 for MovementName in df['MovementName']
+]
+
+df.drop('MovementName', axis=1, inplace=True)
+
+print(df)
+
+X = df.drop('Dancen', axis=1)
+y = df['Dancen']
+'''
+
+# Configuration options
+n_samples = (df.shape[0] - 1) 
+n_features = (df.shape[1] - 1)
+random_state=42
+validation_split = 0.3
+epochs = 200
+
+
+df['Gehen'] = [
+    1 if MovementName == "Gehen" else 0 for MovementName in df['MovementName']
+]
+df['huepfen'] = [
+    1 if MovementName == "huepfen" else 0 for MovementName in df['MovementName']
+]
+df['stehen'] = [
+    1 if MovementName == "stehen" else 0 for MovementName in df['MovementName']
+]
+
+y = df[['Gehen','stehen','huepfen']]
+y.append = df['stehen']
+y.append = df['huepfen']
+
+X = df.drop(['MovementName', 'Gehen', 'stehen', 'huepfen'], axis=1)
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, 
+    test_size=validation_split, random_state=random_state
+)
+
+#print(X_train)
+#print(y_train)
+#np.savetxt("test.csv", X_train, delimiter=',')
+labels = y_test.columns.values
+
+
+tf.random.set_seed(42)
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(1500, activation='relu'),
+    tf.keras.layers.Dense(3000, activation='relu'),
+    tf.keras.layers.Dense(6000, activation='relu'),
+    tf.keras.layers.Dense(3000, activation='relu'),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(3, activation='sigmoid')
+])
+
+model.compile(
+    loss=tf.keras.losses.categorical_crossentropy,
+    optimizer=tf.keras.optimizers.SGD(learning_rate=0.1),
+    metrics=[
+        tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+        tf.keras.metrics.Precision(name='precision'),
+        tf.keras.metrics.Recall(name='recall')
+    ]
+)
+
+history = model.fit(X_train, y_train, epochs=epochs)
+
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+
+rcParams['figure.figsize'] = (18, 8)
+rcParams['axes.spines.top'] = False
+rcParams['axes.spines.right'] = False
+
+plt.plot(
+    np.arange(1, epochs+1), 
+    history.history['loss'], label='Loss'
+)
+plt.plot(
+    np.arange(1, epochs+1), 
+    history.history['accuracy'], label='Accuracy'
+)
+plt.plot(
+    np.arange(1, epochs+1), 
+    history.history['precision'], label='Precision'
+)
+plt.plot(
+    np.arange(1, epochs+1), 
+    history.history['recall'], label='Recall'
+)
+plt.title('Evaluation metrics', size=20)
+plt.xlabel('Epoch', size=14)
+plt.legend()
+
+plt.show()
+
+#print("Prediction:")
+predictions = model.predict(X_test)
+#print (predictions)
+
+
+#print("Prediction Softmax")
+predictions = softmax(predictions, axis=1)
+#print (predictions)
+
+#print("Prediction Final")
+predictions = np.around(predictions)
+#print (predictions)
+
+
+
+print("y_test:")
+print (y_test)
+
+
+from sklearn.metrics import multilabel_confusion_matrix
+print(multilabel_confusion_matrix(y_test, predictions))
