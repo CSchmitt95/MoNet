af586a8c63e70d748d3c9540adc5f8f291d98497
Carlo Schmitt <schmitt@cuba.chi.uni-hannover.de>
2022-02-22 10:26:47 +0100

    Fehlerbehebung

--- Preprocess32.py
+++ Preprocess32.py
@@ -95,12 +95,12 @@ with Bar("Aufnahmen", max=number_of_chunks) as bar:
         bar.next()
 print()
 
-#print("Nulle Windows an erster Quaternion...")
-#for sensor in sensors:
-#    windows = windowed_data.get(sensor)
-#    normalized_windows = PreprocessUtil.normalizeWindows32(windows)
-#    windowed_data.update({ sensor: normalized_windows})
-#print()
+print("Nulle Windows an erster Quaternion...")
+for sensor in sensors:
+    windows = windowed_data.get(sensor)
+    normalized_windows = PreprocessUtil.normalizeWindows32(windows)
+    windowed_data.update({ sensor: normalized_windows})
+print()
 
 #Kombiniere Alle Sensordaten in Kombo-Klasse
 print("Kombiniere windows...")
